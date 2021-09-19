998482b2a04031fb07863d9653a778b6f3f8f9d5
hachi-nico <nicokun48@gmail.com>
2021-09-19 18:26:41 +0700

    add some data

--- src/mock/data.js
+++ src/mock/data.js
@@ -2,26 +2,26 @@ import { nanoid } from 'nanoid';
 
 // HEAD DATA
 export const headData = {
-  title: '', // e.g: 'Name | Developer'
-  lang: '', // e.g: en, es, fr, jp
-  description: '', // e.g: Welcome to my website
+  title: 'Nico | Developer', // e.g: 'Name | Developer'
+  lang: 'en, id', // e.g: en, es, fr, jp
+  description: 'my personal portofolio website', // e.g: Welcome to my website
 };
 
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Nico',
+  subtitle: 'Software Developer',
   cta: '',
 };
 
 // ABOUT DATA
 export const aboutData = {
   img: 'profile.jpg',
-  paragraphOne: '',
-  paragraphTwo: '',
-  paragraphThree: '',
-  resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
+  paragraphOne: 'I like programming, especially on javascript ecosystem.',
+  paragraphTwo: ' I can learn new technology quickly.',
+  paragraphThree: 'I also familiar with development environment in linux.',
+  resume: 'https://drive.google.com/drive/folders/1XPKmPR3PX0_S-JqsA5LtEQH-_6nIUiR-?usp=sharing', // if no resume, the button will not show up
 };
 
 // PROJECTS DATA
@@ -57,9 +57,9 @@ export const projectsData = [
 
 // CONTACT DATA
 export const contactData = {
-  cta: '',
-  btn: '',
-  email: '',
+  cta: 'Email me at nicoapppp@gmail.com or click this button',
+  btn: 'Mail me',
+  email: 'nicoapppp@gmail.com',
 };
 
 // FOOTER DATA
@@ -67,28 +67,13 @@ export const footerData = {
   networks: [
     {
       id: nanoid(),
-      name: 'twitter',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'codepen',
-      url: '',
-    },
-    {
-      id: nanoid(),
-      name: 'linkedin',
-      url: '',
-    },
-    {
-      id: nanoid(),
       name: 'github',
-      url: '',
+      url: 'https://github.com/hachi-nico',
     },
   ],
 };
 
 // Github start/fork buttons
 export const githubButtons = {
-  isEnabled: true, // set to false to disable the GitHub stars/fork buttons
+  isEnabled: false, // set to false to disable the GitHub stars/fork buttons
 };
